05b07d306cd3226f910524418cf3ac221ff07890
awesomemax <cvb.cvb.2017.cvb@gmail.com>
2023-11-11 18:12:00 +0300

    changes

--- src/main/java/com/testovoe/gisbis/controller/RenderController.java
+++ src/main/java/com/testovoe/gisbis/controller/RenderController.java
@@ -1,10 +1,12 @@
 package com.testovoe.gisbis.controller;
 
+import doodle.algebra.generic.BoundingBox;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class RenderController {
 
     @GetMapping("/render")
-    public String renderData(){
+    public String renderData(@RequestParam BoundingBox boundingBox, @RequestParam int width, @RequestParam int height){
         return "hello";
     }
 }
---
+++ src/main/java/com/testovoe/gisbis/model/Render.java
@@ -0,0 +1,7 @@
+package com.testovoe.gisbis.model;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+
+public class Render {
+}
